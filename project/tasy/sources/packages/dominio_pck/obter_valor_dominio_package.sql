cae62d58ee9c01319967668f6501a23edbc3b0f7
Juliano Pezzini <juliano.pezzini@philips.com>
2022-08-03 08:40:45 -0300

    AB#162974 Package state solution

--- project/tasy/sources/packages/dominio_pck/obter_valor_dominio_package.sql
+++ project/tasy/sources/packages/dominio_pck/obter_valor_dominio_package.sql
@@ -4,40 +4,55 @@
 
 SET client_encoding TO 'UTF8';
 
-
-
-
 CREATE OR REPLACE FUNCTION dominio_pck.obter_valor_dominio ( cd_dominio_p bigint, vl_dominio_p text) RETURNS varchar AS $body$
 DECLARE
-
-					
-	chave_w	varchar(100);
 	nr_seq_idioma_w	bigint;
 	
+	vetor_w jsonb;
+	new_value jsonb;
+	chave_w text;
+	raw_value text;
+	ds_valor_dominio_w text;
 BEGIN
-	
 	if (cd_dominio_p IS NOT NULL AND cd_dominio_p::text <> '') and (vl_dominio_p IS NOT NULL AND vl_dominio_p::text <> '') then
 		
-		nr_seq_idioma_w	:= coalesce(philips_param_pck.get_nr_seq_idioma,1);
+		nr_seq_idioma_w	:= coalesce(philips_param_pck.get_nr_seq_idioma(),1);
 		chave_w	:= nr_seq_idioma_w||'_'||cd_dominio_p||'_'||vl_dominio_p;
 		
-		if (current_setting('dominio_pck.vetor_w')::vetorValorDominio.EXISTS(chave_w) and current_setting('dominio_pck.vetor_w')::vetorValorDominio[chave_w](.ds_valor_dominio IS NOT NULL AND .ds_valor_dominio::text <> '')) then
-			return current_setting('dominio_pck.vetor_w')::vetorValorDominio[chave_w].ds_valor_dominio;
+		RAISE NOTICE 'Domain PCK cache before: %', current_setting('dominio_pck.vetor_w', true);
+		raw_value := current_setting('dominio_pck.vetor_w', true);
+		
+		if (raw_value is not null and raw_value <> '') then
+			vetor_w := current_setting('dominio_pck.vetor_w', true)::jsonb;
+			if (vetor_w ? chave_w) then
+				ds_valor_dominio_w := (vetor_w -> chave_w);
+			end if;
+		end if;
+		
+		if (ds_valor_dominio_w IS NOT NULL AND ds_valor_dominio_w::text <> '') then
+			RAISE NOTICE 'Got domain description from cache...';
+			return ds_valor_dominio_w;
 		else
-			current_setting('dominio_pck.vetor_w')::vetorValorDominio[chave_w].cd_dominio		:= cd_dominio_p;
-			current_setting('dominio_pck.vetor_w')::vetorValorDominio[chave_w].vl_dominio		:= vl_dominio_p;
 			begin
-			select	substr(coalesce(ds_valor_dominio_cliente, obter_desc_expressao_idioma(cd_exp_valor_dominio, ds_valor_dominio, nr_seq_idioma_w)), 1, 255)
-			into STRICT 	current_setting('dominio_pck.vetor_w')::vetorValorDominio[chave_w].ds_valor_dominio
-			from 	valor_dominio
-			where 	cd_dominio	= cd_dominio_p
-			and 	vl_dominio	= vl_dominio_p;
+				select	substr(coalesce(ds_valor_dominio_cliente, obter_desc_expressao_idioma(cd_exp_valor_dominio, ds_valor_dominio, nr_seq_idioma_w)), 1, 255)
+				into STRICT ds_valor_dominio_w
+				from 	valor_dominio
+				where 	cd_dominio	= cd_dominio_p
+				and 	vl_dominio	= vl_dominio_p;
+				RAISE NOTICE 'Got domain description from db...';
 			exception
-			      when others then
-				current_setting('dominio_pck.vetor_w')::vetorValorDominio[chave_w].ds_valor_dominio := null;
-			    end;
-
-			return current_setting('dominio_pck.vetor_w')::vetorValorDominio[chave_w].ds_valor_dominio;
+				when others then
+					ds_valor_dominio_w := null;
+			end;
+			new_value := jsonb_build_object(chave_w, ds_valor_dominio_w);
+			if (vetor_w is null) then
+				vetor_w := new_value;
+			else
+				vetor_w := vetor_w || new_value;
+			end if;
+			perform set_config('dominio_pck.vetor_w', vetor_w::text, false);
+			RAISE NOTICE 'Domain PCK cache after: %', current_setting('dominio_pck.vetor_w', true);
+			return ds_valor_dominio_w;
 		end if;
 	
 	end if;
@@ -50,4 +65,5 @@ $body$
 LANGUAGE PLPGSQL
 SECURITY DEFINER
 ;
+
 -- REVOKE ALL ON FUNCTION dominio_pck.obter_valor_dominio ( cd_dominio_p bigint, vl_dominio_p text) FROM PUBLIC;
