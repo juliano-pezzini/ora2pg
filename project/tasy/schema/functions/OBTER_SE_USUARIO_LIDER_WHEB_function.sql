dbe3c99a56c2f933d9c6901ffc7a98f70329895a
Juliano Pezzini <juliano.pezzini@philips.com>
2022-08-10 21:14:58 -0300

    AB#164293 OBTER_SE_USUARIO_LIDER_WHEB

--- project/tasy/schema/functions/OBTER_SE_USUARIO_LIDER_WHEB_function.sql
+++ project/tasy/schema/functions/OBTER_SE_USUARIO_LIDER_WHEB_function.sql
@@ -4,33 +4,28 @@
 
 SET client_encoding TO 'UTF8';
 
-
-
-
-
 CREATE OR REPLACE FUNCTION obter_se_usuario_lider_wheb (nm_usuario_p text) RETURNS varchar AS $body$
 DECLARE
-
-
     ie_lider_w varchar(1) := 'N';
 
-
 BEGIN
-    $if $$tasy_local_dict=true $then
-    return 'S';
-    $else
-    if (nm_usuario_p IS NOT NULL AND nm_usuario_p::text <> '') then
-        begin
-            EXECUTE 'begin verifica_cargo_usuario@whebl01_dbcorp(:nm_usuario_p,''A,G''); end;'
-                using nm_usuario_p;
-            ie_lider_w := 'S';
-        exception
-            when others then
-                ie_lider_w := 'N';
-        end;
-    end if;
-    return ie_lider_w;
-    $end
+  return 'N';
+
+    -- $if $$tasy_local_dict=true $then
+    -- return 'S';
+    -- $else
+    -- if (nm_usuario_p IS NOT NULL AND nm_usuario_p::text <> '') then
+    --     begin
+    --         EXECUTE 'begin verifica_cargo_usuario@whebl01_dbcorp(:nm_usuario_p,''A,G''); end;'
+    --             using nm_usuario_p;
+    --         ie_lider_w := 'S';
+    --     exception
+    --         when others then
+    --             ie_lider_w := 'N';
+    --     end;
+    -- end if;
+    -- return ie_lider_w;
+    -- $end
 end;
 $body$
 LANGUAGE PLPGSQL
